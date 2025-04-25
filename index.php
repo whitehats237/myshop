e2c45c7aa73f5ea4e92b621aeeeccbd36502fd02
whitehats237 <joebilly072004@gmail.com>
2025-04-25 21:59:41 +0200

    new commits of this day 😭

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="myshop.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class="image_header">
+    <img src="logo.jpg" alt="" height="200" width="200">
+    <h2>Welcome to My Shop</h2>
+    </div>
+    <div class="header_nav">
+        <nav>
+            <ul>
+                </li><a href="index.php">Home</a></l>
+                <li><a href="Inscription.php">Inscription</a></li>
+                <li><a href="products.php">Products</a></li>
+                <li><a href="about.php">About Us</a></li>
+                <li><a href="contact.php">Contact Us</a></li>
+            </ul>
+            
+        </nav>
+    </div>
+    
+
+</body>
+</html>
